6002c7c22cc29c835916e4c0cf520cc1589e2269
keatonb <keatonbell@utexas.edu>
2020-05-18 17:00:37 -0700

    include extinction correction option

--- WDGaiaRadius.py
+++ WDGaiaRadius.py
@@ -20,14 +20,16 @@ import os
 
 from BergeronToPandas import BergeronToPandas
 
-def WDGaiaRadius(ra,dec,teff,teff_err=None,searchradius=5.0,modelmass=0.6,modelpath='AllModels/'):
+def WDGaiaRadius(ra,dec,teff,teff_err=None,searchradius=5.0,modelmass=0.6,
+                 modelpath='AllModels/',A_G=0):
     """Estimate WD Radius from Gaia astrometry.
     
     Queries for Gaia data and distances from Bailer-Jones et al. (2018, ApJ, 156, 2).
     Scales Bergeron et al. DA (no DBs currently) cooling models 
     (http://www.astro.umontreal.ca/~bergeron/CoolingModels/)
     until the radius reproduces the Gaia magnitude at the parallactic distance.
-    Does not account for extinction or reddening.  
+    Corrects for extinction if coefficient A_G is provided in magnitudes, 
+    following the prescription of Gentile-Fusillo et al. (2019, MNRAS, 482, 4570).
     Uses https://github.com/keatonb/BergeronToPandas to read in Bergeron models.
     If error on Teff given, includes contribution roughly in quadrature.
     Returns measurement and lower, upper bounds defined by Bailer-Jones et al. 
@@ -50,17 +52,26 @@ def WDGaiaRadius(ra,dec,teff,teff_err=None,searchradius=5.0,modelmass=0.6,modelp
         #Accept nearest
         sourceid = r["source_id"][0]
         Gmag = r["phot_g_mean_mag"][0]
-        print("nearest has Gaia G mag of {}".format(Gmag))
+        print("Nearest has Gaia G mag of {}.".format(Gmag))
+        b = r["b"][0] #Galactic latitude in degrees
+        #Extinction correction following Gentile-Fusillo et al. 2019
+        parallax = r["parallax"][0]*1e-3 #arcsec
+        extcorr = -A_G*(1. - np.exp(-np.sin(np.abs(b*np.pi/180)) / (200*parallax)))
+        Gmag += extcorr
+        print("Extinction correction of {} then applied to get G = {} mag.".format(extcorr,Gmag))
+        
+        
         #Then get Bailer-Jones distance
         v = Vizier(columns=['*'],
                 column_filters={"Source":str(sourceid)}).get_catalogs('I/347')
         dist = np.array([v[0]["rest"][0],v[0]["b_rest"][0],v[0]["B_rest"][0]])
-
+        print("Distance constraints from Bailer-Jones et al: {}".format(dist))
+        
         #Referencing Bergeron cooling models
         modelfile = os.path.join(modelpath, "Table_Mass_{:.1f}".format(modelmass))
         print("Referencing Bergeron model file at "+modelfile)
         
-        models = BergeronToPandas(modelfile).values()[0]
+        models = list(BergeronToPandas(modelfile).values())[0]
         
         #Constants
         G = 6.67259e-8 #cm3 g-1 s-2
